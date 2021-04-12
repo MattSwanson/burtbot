7a70f2178906240e6a50ca4a8669de494cda783e
Matt Swanson <swanny11@gmail.com>
2021-04-11 21:34:32 -0500

    flesh out shout out

--- commands/shoutout.go
+++ commands/shoutout.go
@@ -10,8 +10,9 @@ import (
 )
 
 type Shoutout struct {
-	TcpChannel   chan string
-	TwitchClient *TwitchAuthClient
+	TcpChannel     chan string
+	TwitchClient   *TwitchAuthClient
+	customMessages map[string]string // key is username, value is a message to display
 }
 
 func (s *Shoutout) Init() {
@@ -22,18 +23,40 @@ func (s *Shoutout) Run(client *twitch.Client, msg twitch.PrivateMessage) {
 	if !isMod(msg.User) {
 		return
 	}
+	if !s.TwitchClient.GetAuthStatus() {
+		client.Say(msg.Channel, "I'd shout them out or whatever but I don't have \"ACCESS\" to the info... hint hint.")
+		return
+	}
 	args := strings.Fields(strings.ToLower(msg.Message))
 	if len(args) < 2 {
 		return
 	}
 	u := s.TwitchClient.GetUser(args[1])
-	fmt.Println(u)
+	if u.UserID == "" {
+		client.Say(msg.Channel, "Sorry, I don't shout out non-existant users. Not for free at least.")
+		return
+	}
+	ci := s.TwitchClient.GetChannelInfo(u.UserID)
+	var game string
+	if ci.GameName == "" {
+		game = "<REDACTED>"
+	} else {
+		game = ci.GameName
+	}
 	r := rand.Intn(100)
-	if r < 40 {
+	if len(args) == 3 {
+		if args[2] == "please" || args[2] == "plz" {
+			client.Say(msg.Channel, "Fine...")
+			client.Say(msg.Channel, fmt.Sprintf("Check out %s on their twitch channel: http://twitch.tv/%[1]s", u.DisplayName))
+			client.Say(msg.Channel, fmt.Sprintf("They were last seen streaming %s. Whatever that is.", game))
+			return
+		}
+	}
+	if r < 80 {
 		client.Say(msg.Channel, "Nah. Maybe some other time.")
 	} else {
-		client.Say(msg.Channel, fmt.Sprintf("Check out %s on their twitch channel. I'm sure you can find it yourself.", args[1]))
-		client.Say(msg.Channel, "If they even have one... I'm too lazy to validate it.")
+		client.Say(msg.Channel, fmt.Sprintf("CHECK OUT %s ON AT http://twitch.tv/%[1]s", u.DisplayName))
+		client.Say(msg.Channel, fmt.Sprintf("THEY WERE LAST SEEN STREAMING %s. WHATEVER THAT IS.", game))
 	}
 }
 
