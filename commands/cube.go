5ab47c604a7f6991f9d775e5a8fa0606ecfc2763
Matt Swanson <swanny11@gmail.com>
2024-02-20 20:07:16 +0000

    add cube count to cube and fmt it

--- commands/cube.go
+++ commands/cube.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Cube struct{}
+
 var cube *Cube = &Cube{}
 var cubeRunning bool
 
@@ -28,28 +29,30 @@ func (c *Cube) Run(msg twitch.PrivateMessage) {
 	}
 
 	switch args[1] {
-		case "move":
-			if len(args) < 3 {
-				return
-			}
-			if !validateMoves(args[2]) {
-				comm.ToChat(msg.Channel, "Invalid moves")
-				return
-			}
-			comm.ToOverlay(fmt.Sprintf("cube move %s", args[2]))
-		case "start":
-				comm.ToOverlay("cube start")
-		case "stop":
-				comm.ToOverlay("cube stop")
-		case "reset":
-			comm.ToOverlay("cube reset")
-		case "shuffle":
-			comm.ToOverlay("cube shuffle")
+	case "move":
+		if len(args) < 3 {
+			return
+		}
+		if !validateMoves(args[2]) {
+			comm.ToChat(msg.Channel, "Invalid moves")
+			return
+		}
+		comm.ToOverlay(fmt.Sprintf("cube move %s", args[2]))
+	case "start":
+		comm.ToOverlay("cube start")
+	case "stop":
+		comm.ToOverlay("cube stop")
+	case "reset":
+		comm.ToOverlay("cube reset")
+	case "shuffle":
+		comm.ToOverlay("cube shuffle")
+	case "movecount":
+		comm.ToOverlay("cube movecount")
 	}
 }
 
 func (c *Cube) PostInit() {
-	
+
 }
 
 func (c *Cube) Help() []string {
@@ -60,14 +63,14 @@ func (c *Cube) Help() []string {
 }
 
 func validateMoves(moves string) bool {
-	for i := 0; i < len(moves); i++{
+	for i := 0; i < len(moves); i++ {
 		// is ch in the valid move slice?
 		var found bool
 		for j := 0; j < len(validMoveChars); j++ {
-			 if moves[i] == validMoveChars[j] {
+			if moves[i] == validMoveChars[j] {
 				found = true
 				break
-			 }
+			}
 		}
 		if !found {
 			return false
