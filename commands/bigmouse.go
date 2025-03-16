0e52015155836b1e7a689504a02f9c63c94d8d9b
Matt Swanson <swanny11@gmail.com>
2025-03-16 13:03:08 +0000

    update bigmouse add cooldown

--- commands/bigmouse.go
+++ commands/bigmouse.go
@@ -1,13 +1,25 @@
 package commands
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/MattSwanson/burtbot/comm"
 	"github.com/gempir/go-twitch-irc/v2"
 )
 
-type BigMouse struct {}
+type BigMouse struct {
+	lastUse time.Time
+}
 
-var bigMouse *BigMouse = &BigMouse{}
+const (
+	bigMouseCooldown = 600 // seconds
+	duration         = 60  // seconds
+)
+
+var (
+	bigMouse *BigMouse = &BigMouse{}
+)
 
 func init() {
 	RegisterCommand("bigmouse", bigMouse)
@@ -18,7 +30,12 @@ func (m *BigMouse) PostInit() {
 }
 
 func (m *BigMouse) Run(msg twitch.PrivateMessage) {
-	comm.ToOverlay("bigmouse")
+	if timeSince := time.Since(m.lastUse).Seconds(); timeSince <= float64(bigMouseCooldown) {
+		comm.ToChat(msg.Channel, fmt.Sprintf("Bigmouse is on cooldown for another %d seconds. Patience...", bigMouseCooldown-int(timeSince)))
+		return
+	}
+	m.lastUse = time.Now()
+	comm.ToOverlay(fmt.Sprintf("bigmouse %d", duration))
 }
 
 func (m *BigMouse) Help() []string {
