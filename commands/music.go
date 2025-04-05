49fb38e2eace523990d4098c90bcc517720283e3
Matt Swanson <swanny11@gmail.com>
2025-04-05 18:41:44 +0000

    some refactoring on music

--- commands/music.go
+++ commands/music.go
@@ -143,192 +143,201 @@ func (m *Music) Run(msg twitch.PrivateMessage) {
 		return
 	}
 
-	if args[1] == "current" {
-		/*	cp, err := m.SpotifyClient.PlayerCurrentlyPlaying()
-			if !cp.Playing {
-				comm.ToChat(msg.Channel, "Not playing music right now")
-				return
-			}
-			if err != nil {
-				log.Println("SpotifyClient err: ", err.Error())
-				comm.ToChat(msg.Channel, "Could not get playback data atm.")
-				return
-			}
-			artists := ""
-			for k, v := range cp.Item.Artists {
-				artists += v.Name
-				if k != len(cp.Item.Artists)-1 {
-					artists += ", "
-				}
-			}
-			retMsg := fmt.Sprintf(`Current Song: "%s" by %s. Listen on Spotify: %s`, cp.Item.Name, artists, cp.Item.ExternalURLs["spotify"])
-			comm.ToChat(msg.Channel, retMsg)
-			return
-		*/
-		if nowPlayingRequest == nil {
-			comm.ToChat(msg.Channel, "There is no song currently playing.")
-			return
-		}
-
-		artists := strings.Join(nowPlayingRequest.SongArtists, ",")
-		text := fmt.Sprintf(`Current Song: "%s" by %s. As requested by %s - Link: %s`, nowPlayingRequest.SongTitle, artists, nowPlayingRequest.User, nowPlayingRequest.SongLink)
-		comm.ToChat(msg.Channel, text)
-		return
+	switch args[1] {
+	case "current":
+		m.writeNowPlayingSongToChat(args, msg)
+	case "last":
+		m.writeLastPlayedSongToChat(args, msg)
+	case "nptext":
+		m.setNowPlayingText(args, msg)
+	case "previous":
+		m.playPreviousSong(args, msg)
+	case "request":
+		m.songRequest(args, msg)
+	case "skip":
+		m.skipCurrentSong(args, msg)
 	}
+}
 
-	if args[1] == "last" {
-		/*
-			rp, err := m.SpotifyClient.PlayerRecentlyPlayed()
-			if err != nil {
-				log.Println("SpotifyClient err: ", err.Error())
-				comm.ToChat(msg.Channel, "Could not get playback data atm.")
-				return
-			}
-			last := rp[0].Track
-			artists := ""
-			for k, v := range last.Artists {
-				artists += v.Name
-				if k != len(last.Artists)-1 {
-					artists += ", "
-				}
-			}
-			retMsg := fmt.Sprintf(`Last Song: "%s" by %s. Listen on Spotify: %s`, last.Name, artists, last.ExternalURLs["spotify"])
-			comm.ToChat(msg.Channel, retMsg)
-			return
-		*/
-		if len(lastRequests) == 0 {
-			comm.ToChat(msg.Channel, "There is no request history.")
+func (m *Music) playPreviousSong(args []string, msg twitch.PrivateMessage) {
+	/*
+		if GetTokenCount(msg.User).Cmp(big.NewInt(previousCost)) == -1 {
+			comm.ToChat(msg.Channel, fmt.Sprintf("@%s you don't have enough tokens to return to the past.", msg.User.DisplayName))
 			return
 		}
 
-		lastSong := lastRequests[0]
-		artists := strings.Join(lastSong.SongArtists, ",")
-		text := fmt.Sprintf(`Last Song: "%s" by %s. As Requested by %s - Link: %s`, lastSong.SongTitle, artists, lastSong.User, lastSong.SongLink)
-		comm.ToChat(msg.Channel, text)
-		return
-	}
-
-	if args[1] == "request" {
-
-		if args[2] == "enable" && IsMod(msg.User) {
-			isAcceptingRequests = true
-			comm.ToChat(msg.Channel, "Song requests have been enabled. Use !sr <song link> to request a song.")
-			return
-		}
-
-		if args[2] == "disable" && IsMod(msg.User) {
-			isAcceptingRequests = false
-			comm.ToChat(msg.Channel, "The song request queue has been closed. Go home.")
-			return
-		}
-
-		if !isAcceptingRequests {
-			comm.ToChat(msg.Channel, "Sorry, requests are not enabled at the moment. Please try again some other century when requests are actually enabled. Thanks for trying though. Your effort is appreciated.")
-			return
-		}
-
-		if len(args) < 3 {
-			return
-		}
-
-		req, err := processRequest(args[2], msg.User.DisplayName)
+		err := m.SpotifyClient.Previous()
 		if err != nil {
-			comm.ToChat(msg.Channel, "Error adding request to the queue, please try again later.")
-			return
-		}
-		duration := 0
-		for _, request := range requestQueue {
-			duration += request.Duration
-		}
-		duration += 60_000 - (duration % 60_000)
-		duration /= 60_000
-		mins := duration % 60
-		hours := duration / 60
-		timeToPlay := fmt.Sprintf("%dmin", mins)
-		if hours > 0 {
-			timeToPlay = fmt.Sprintf("%dhr %s", hours, timeToPlay)
-		}
-		requestQueue = append(requestQueue, req)
-		comm.ToChat(msg.Channel, fmt.Sprintf("@%s: %s - %s has been added at #%d in the queue. Played in approx. %s", msg.User.DisplayName, req.SongTitle, strings.Join(req.SongArtists, ","), len(requestQueue), timeToPlay))
-		saveQueueToFile()
-	}
-
-	if args[1] == "skip" {
-		/*
-			if GetTokenCount(msg.User).Cmp(big.NewInt(skipCost)) == -1 {
-				comm.ToChat(msg.Channel, fmt.Sprintf("@%s you don't have enough tokens to skip this song. Deal with it.", msg.User.DisplayName))
-				return
-			}
-
-			err := m.SpotifyClient.Next()
-			if err != nil {
-				log.Println("Could not skip track - ", err.Error())
-				comm.ToChat(msg.Channel, "Sorry I failed to skip the track, I won't take your tokens. Though I could. If I wanted to.")
-				return
-			}
-
-			DeductTokens(msg.User.DisplayName, big.NewInt(skipCost))
-			tokensLeft := GetTokenCount(msg.User)
-			plural := ""
-			if tokensLeft.Cmp(big.NewInt(1)) == 1 {
-				plural = "s"
-			}
-			comm.ToChat(msg.Channel, fmt.Sprintf("Are you happy @%s? You skipped everyone's favorite song...", msg.User.DisplayName))
-			if tokensLeft.Cmp(big.NewInt(0)) == 1 {
-				comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you only have %d token%s left", msg.User.DisplayName, GetTokenCount(msg.User), plural))
-			} else {
-				comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you have no tokens left. Sad.", msg.User.DisplayName))
-			}
-		*/
-		comm.ToChat(msg.Channel, "Song skipping has been disabled for the time being. Good job for knowing this command existed in the first place.")
-
-		return
-	}
-
-	if args[1] == "previous" {
-		/*
-			if GetTokenCount(msg.User).Cmp(big.NewInt(previousCost)) == -1 {
-				comm.ToChat(msg.Channel, fmt.Sprintf("@%s you don't have enough tokens to return to the past.", msg.User.DisplayName))
-				return
-			}
-
-			err := m.SpotifyClient.Previous()
-			if err != nil {
-				log.Println("Could not go to previous track - ", err.Error())
-				comm.ToChat(msg.Channel, "Couldn't go back to the previous track. Maybe it never existesd. For all we know the universe started last Thursday.")
-				return
-			}
-
-			DeductTokens(msg.User.DisplayName, big.NewInt(previousCost))
-			tokensLeft := GetTokenCount(msg.User)
-			plural := ""
-			if tokensLeft.Cmp(big.NewInt(1)) == 1 {
-				plural = "s"
-			}
-			comm.ToChat(msg.Channel, fmt.Sprintf("Okay @%s, I guess we have to go back to the last song.", msg.User.DisplayName))
-			if tokensLeft.Cmp(big.NewInt(0)) == 1 {
-				comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you only have %d token%s left", msg.User.DisplayName, GetTokenCount(msg.User), plural))
-			} else {
-				comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you have no tokens left. Sad.", msg.User.DisplayName))
-			}
-		*/
-		comm.ToChat(msg.Channel, "We can not return to the past.")
-		return
-	}
-
-	if args[1] == "nptext" && IsMod(msg.User) {
-		if len(args) < 3 {
+			log.Println("Could not go to previous track - ", err.Error())
+			comm.ToChat(msg.Channel, "Couldn't go back to the previous track. Maybe it never existesd. For all we know the universe started last Thursday.")
 			return
 		}
 
-		if args[2] == "top" || args[2] == "bottom" {
-			comm.ToOverlay(fmt.Sprintf("nptext %s", args[2]))
+		DeductTokens(msg.User.DisplayName, big.NewInt(previousCost))
+		tokensLeft := GetTokenCount(msg.User)
+		plural := ""
+		if tokensLeft.Cmp(big.NewInt(1)) == 1 {
+			plural = "s"
+		}
+		comm.ToChat(msg.Channel, fmt.Sprintf("Okay @%s, I guess we have to go back to the last song.", msg.User.DisplayName))
+		if tokensLeft.Cmp(big.NewInt(0)) == 1 {
+			comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you only have %d token%s left", msg.User.DisplayName, GetTokenCount(msg.User), plural))
+		} else {
+			comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you have no tokens left. Sad.", msg.User.DisplayName))
+		}
+	*/
+	comm.ToChat(msg.Channel, "We can not return to the past.")
+}
+
+func (m *Music) setNowPlayingText(args []string, msg twitch.PrivateMessage) {
+	if !IsMod(msg.User) || len(args) < 3 {
+		return
+	}
+	if args[2] == "top" || args[2] == "bottom" {
+		comm.ToOverlay(fmt.Sprintf("nptext %s", args[2]))
+	}
+
+	if args[2] == "off" {
+		comm.ToOverlay("nowplaying off")
+	}
+
+}
+
+func (m *Music) skipCurrentSong(args []string, msg twitch.PrivateMessage) {
+	/*
+		if GetTokenCount(msg.User).Cmp(big.NewInt(skipCost)) == -1 {
+			comm.ToChat(msg.Channel, fmt.Sprintf("@%s you don't have enough tokens to skip this song. Deal with it.", msg.User.DisplayName))
+			return
 		}
 
-		if args[2] == "off" {
-			comm.ToOverlay("nowplaying off")
+		err := m.SpotifyClient.Next()
+		if err != nil {
+			log.Println("Could not skip track - ", err.Error())
+			comm.ToChat(msg.Channel, "Sorry I failed to skip the track, I won't take your tokens. Though I could. If I wanted to.")
+			return
 		}
+
+		DeductTokens(msg.User.DisplayName, big.NewInt(skipCost))
+		tokensLeft := GetTokenCount(msg.User)
+		plural := ""
+		if tokensLeft.Cmp(big.NewInt(1)) == 1 {
+			plural = "s"
+		}
+		comm.ToChat(msg.Channel, fmt.Sprintf("Are you happy @%s? You skipped everyone's favorite song...", msg.User.DisplayName))
+		if tokensLeft.Cmp(big.NewInt(0)) == 1 {
+			comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you only have %d token%s left", msg.User.DisplayName, GetTokenCount(msg.User), plural))
+		} else {
+			comm.ToChat(msg.Channel, fmt.Sprintf("@%s, also, you have no tokens left. Sad.", msg.User.DisplayName))
+		}
+	*/
+	comm.ToChat(msg.Channel, "Song skipping has been disabled for the time being. Good job for knowing this command existed in the first place.")
+}
+
+func (m *Music) songRequest(args []string, msg twitch.PrivateMessage) {
+	if len(args) < 3 {
+		return
 	}
+
+	if args[2] == "enable" && IsMod(msg.User) {
+		isAcceptingRequests = true
+		comm.ToChat(msg.Channel, "Song requests have been enabled. Use !sr <song link> to request a song.")
+		return
+	}
+
+	if args[2] == "disable" && IsMod(msg.User) {
+		isAcceptingRequests = false
+		comm.ToChat(msg.Channel, "The song request queue has been closed. Go home.")
+		return
+	}
+
+	if !isAcceptingRequests {
+		comm.ToChat(msg.Channel, "Sorry, requests are not enabled at the moment. Please try again some other century when requests are actually enabled. Thanks for trying though. Your effort is appreciated.")
+		return
+	}
+
+	req, err := processRequest(args[2], msg.User.DisplayName)
+	if err != nil {
+		comm.ToChat(msg.Channel, "Error adding request to the queue, please try again later.")
+		return
+	}
+	duration := 0
+	for _, request := range requestQueue {
+		duration += request.Duration
+	}
+	duration += 60_000 - (duration % 60_000)
+	duration /= 60_000
+	mins := duration % 60
+	hours := duration / 60
+	timeToPlay := fmt.Sprintf("%dmin", mins)
+	if hours > 0 {
+		timeToPlay = fmt.Sprintf("%dhr %s", hours, timeToPlay)
+	}
+	requestQueue = append(requestQueue, req)
+	comm.ToChat(msg.Channel, fmt.Sprintf("@%s: %s - %s has been added at #%d in the queue. Played in approx. %s", msg.User.DisplayName, req.SongTitle, strings.Join(req.SongArtists, ","), len(requestQueue), timeToPlay))
+	saveQueueToFile()
+}
+
+func (m *Music) writeLastPlayedSongToChat(args []string, msg twitch.PrivateMessage) {
+	/*
+		rp, err := m.SpotifyClient.PlayerRecentlyPlayed()
+		if err != nil {
+			log.Println("SpotifyClient err: ", err.Error())
+			comm.ToChat(msg.Channel, "Could not get playback data atm.")
+			return
+		}
+		last := rp[0].Track
+		artists := ""
+		for k, v := range last.Artists {
+			artists += v.Name
+			if k != len(last.Artists)-1 {
+				artists += ", "
+			}
+		}
+		retMsg := fmt.Sprintf(`Last Song: "%s" by %s. Listen on Spotify: %s`, last.Name, artists, last.ExternalURLs["spotify"])
+		comm.ToChat(msg.Channel, retMsg)
+		return
+	*/
+	if len(lastRequests) == 0 {
+		comm.ToChat(msg.Channel, "There is no request history.")
+		return
+	}
+
+	lastSong := lastRequests[0]
+	artists := strings.Join(lastSong.SongArtists, ",")
+	text := fmt.Sprintf(`Last Song: "%s" by %s. As Requested by %s - Link: %s`, lastSong.SongTitle, artists, lastSong.User, lastSong.SongLink)
+	comm.ToChat(msg.Channel, text)
+}
+
+func (m *Music) writeNowPlayingSongToChat(args []string, msg twitch.PrivateMessage) {
+	/*	cp, err := m.SpotifyClient.PlayerCurrentlyPlaying()
+		if !cp.Playing {
+			comm.ToChat(msg.Channel, "Not playing music right now")
+			return
+		}
+		if err != nil {
+			log.Println("SpotifyClient err: ", err.Error())
+			comm.ToChat(msg.Channel, "Could not get playback data atm.")
+			return
+		}
+		artists := ""
+		for k, v := range cp.Item.Artists {
+			artists += v.Name
+			if k != len(cp.Item.Artists)-1 {
+				artists += ", "
+			}
+		}
+		retMsg := fmt.Sprintf(`Current Song: "%s" by %s. Listen on Spotify: %s`, cp.Item.Name, artists, cp.Item.ExternalURLs["spotify"])
+		comm.ToChat(msg.Channel, retMsg)
+		return
+	*/
+	if nowPlayingRequest == nil {
+		comm.ToChat(msg.Channel, "There is no song currently playing.")
+		return
+	}
+
+	artists := strings.Join(nowPlayingRequest.SongArtists, ",")
+	text := fmt.Sprintf(`Current Song: "%s" by %s. As requested by %s - Link: %s`, nowPlayingRequest.SongTitle, artists, nowPlayingRequest.User, nowPlayingRequest.SongLink)
+	comm.ToChat(msg.Channel, text)
 }
 
 func (m *Music) getCurrentTrackTitle() (string, bool) {
